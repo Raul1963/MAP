29bd8710c4d0bb9cef7eb1e06f3da594dd757836
Raul1963 <raulmarginean1963@gmail.com>
2024-10-02 12:41:44 +0300

    Initial

--- S1/src/StringOps.java
+++ S1/src/StringOps.java
@@ -7,6 +7,12 @@ public class StringOps {
         return reversed;
     }
     public int wordcount(String str){
-
+        int cnt=0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)==' '){
+                cnt++;
+            }
+        }
+        return cnt+1;
     }
 }
